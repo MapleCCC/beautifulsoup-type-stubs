f7a7d8db41188fa32f4bf6e4061e245fa9e0e593
MapleCCC <littlelittlemaple@gmail.com>
2019-11-24 00:25:08 +0800

    No need to extract away a standalone module for single name.

--- bs4/auxiliary_typing.pyi
+++
@@ -1,8 +0,0 @@
-from typing import *
-
-from .element import Tag
-
-try:
-    Filter = Union[AnyStr, List[AnyStr], Pattern, Callable[[Tag], bool], Literal[bool]]
-except NameError:
-    Filter = Union[AnyStr, List[AnyStr], Pattern, Callable[[Tag], bool], bool]
